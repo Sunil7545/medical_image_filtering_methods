6f06c2aceca6c1e1ccf00d38e30e83f284ec0117
Sunil Yadav <sunil7545@gmail.com>
2020-06-17 12:56:24 +0200

    Add files via upload

--- filtering_methods.py
+++ filtering_methods.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
+from skimage import io
+from skimage.util import compare_images
 
 
 def log_transformation(input_image):
@@ -8,25 +10,34 @@ def log_transformation(input_image):
     return np.log1p(input_image).astype(np.float32)
 
 
-def mean_filtering(input_image, window_size=(13, 13)):
-    return cv2.blur(input_image, window_size)
+def mean_filtering(input_image, window_size=(15, 15)):
+    filtered_image = cv2.blur(input_image, window_size)
+    filtered_image = np.nan_to_num(filtered_image)
+    return filtered_image
 
 
-def gaussian_filtering(input_image, window_size=(13, 13), sigma_x=6, sigma_y=6):
-    return cv2.GaussianBlur(input_image, window_size, sigma_x, sigma_y)
+def gaussian_filtering(input_image, window_size=(15, 15), sigma_x=6, sigma_y=8):
+    filtered_image = cv2.GaussianBlur(input_image, window_size, sigma_x, sigma_y)
+    filtered_image = np.nan_to_num(filtered_image)
+    return filtered_image
 
 
-def median_filtering(input_image, window_size=13):
-    return cv2.medianBlur(input_image, window_size)
+def median_filtering(input_image, window_size=15):
+    filtered_image = np.array(cv2.medianBlur(input_image, window_size))
+    filtered_image = np.nan_to_num(filtered_image)
+    return filtered_image
 
 
 def bilateral_filtering(input_image, window_size=13, sigma_color=3, sigma_space=5):
-    return cv2.bilateralFilter(input_image, window_size, sigma_color, sigma_space)
+    filtered_image = cv2.bilateralFilter(input_image, window_size, sigma_color, sigma_space)
+    filtered_image = np.nan_to_num(filtered_image)
+    return filtered_image
 
 
 def exp_transformation(input_image):
     # exponential transformation
-    return np.expm1(input_image)
+    filtered_image = np.expm1(input_image)
+    return filtered_image*(255/np.max(filtered_image))
 
 
 def signal_to_noise(image_input, axis=0, ddof=0):
@@ -36,27 +47,41 @@ def signal_to_noise(image_input, axis=0, ddof=0):
     return np.mean(np.where(sd == 0, 0, m/sd))
 
 
+def speckle_generator(image):
+    row, col = image.shape
+    gauss = np.random.randn(row, col)
+    gauss = gauss.reshape(row, col)
+    noisy = image + 0.4*image * gauss
+    return noisy*(255/np.max(image))
+
+
 def image_filtering_anisotropic():
     """
     Anisotropic image filtering methods
     """
 
-    image_retina = plt.imread("retina_input.jpg")
+    # reading, corrupting image with noise, and removing nan, negative values.
+    image_ref = io.imread('input.jpg', as_gray=True)
+    image_retina = speckle_generator(image_ref)
+    image_retina = np.nan_to_num(image_retina)
+    image_retina[image_retina < 0] = 1
     print("Input Image SNR: " + str(signal_to_noise(image_retina)))
 
-    fig, axs = plt.subplots(3, 1, figsize=(4, 8))
+    fig, axs = plt.subplots(3, 1, figsize=(4, 9.5))
     axs[0].imshow(image_retina, cmap='gray', vmin=0, vmax=255)
     axs[0].set_title("Input")
 
     log_input = log_transformation(image_retina)
     median_output = exp_transformation(median_filtering(log_input))
+    compute_diff = compare_images(image_retina, median_output, method='checkerboard', n_tiles=(4, 4))
+    io.imsave("diff_input_output.jpg", compute_diff)
     print("Median SNR: " + str(signal_to_noise(median_output)))
-    axs[1].imshow(median_output, cmap='gray', vmin=0, vmax=255)
+    axs[1].imshow(median_output, cmap='gray')
     axs[1].set_title("Median Filtering")
 
     bil_output = exp_transformation(bilateral_filtering(log_input))
     print("Bilateral SNR: " + str(signal_to_noise(bil_output)))
-    axs[2].imshow(bil_output, cmap='gray', vmin=0, vmax=255)
+    axs[2].imshow(bil_output, cmap='gray')
     axs[2].set_title("Bilateral Filtering")
 
     for j in range(3):
@@ -71,19 +96,23 @@ def image_filtering_isotropic():
     Isotropic methods for image filtering
     """
 
-    image_retina = plt.imread("retina_input.jpg")
+    # image_retina = plt.imread("retina_input.jpg")
+    image_ref = io.imread('input.jpg', as_gray=True)
+    image_retina = speckle_generator(image_ref)
+    image_retina = np.nan_to_num(image_retina)
+    image_retina[image_retina < 0] = 1
 
-    fig, axs = plt.subplots(3, 1, figsize=(4, 8))
+    fig, axs = plt.subplots(3, 1, figsize=(4, 9.5))
     axs[0].imshow(image_retina, cmap='gray', vmin=0, vmax=255)
     axs[0].set_title("Input")
 
     log_input = log_transformation(image_retina)
-    mean_output = exp_transformation(median_filtering(log_input))
-    axs[1].imshow(mean_output, cmap='gray', vmin=0, vmax=255)
+    mean_output = exp_transformation(mean_filtering(log_input))
+    axs[1].imshow(mean_output, cmap='gray')
     axs[1].set_title("Mean Filtering")
 
     gauss_mean = exp_transformation(bilateral_filtering(log_input))
-    axs[2].imshow(gauss_mean, cmap='gray', vmin=0, vmax=255)
+    axs[2].imshow(gauss_mean, cmap='gray')
     axs[2].set_title("Gaussian Filtering")
 
     for j in range(3):
